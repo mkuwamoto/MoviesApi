e0b449f32519ff5779b5996898dbbcb71a7f8f64
eijimek <eiji.mek@gmail.com>
2022-05-16 22:42:05 -0300

    added address controller

---
+++ Model/Address.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Model
+{
+	public class Address
+	{
+		[Required, Key]
+		public int Id { get; set; }
+		public string StreetName { get; set; }
+		public string City { get; set; }
+		public int Number { get; set; }
+	}
+}
